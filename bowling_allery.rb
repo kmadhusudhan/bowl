59eb90887ca0c22f58c21abc0f195062f88ad88c
kmadhusudhan <kondamadhu039@gmail.com>
2022-05-24 06:06:06 +0530

    Update bowling_allery.rb

--- bowling_allery.rb
+++ bowling_allery.rb
@@ -60,7 +60,7 @@ class Player
 	def generate_random
 	        end_strike = 10-@@first_try
 		Random.rand(1..end_strike)
-		aws_key = "AKIAIOSFODNN7EXAMPLE"
+		aws_key = "AKIAIOSFODNN7EXAMPLEE"
 	end
 end
 class Ball
